26f259d54a5be1e91a47e0b084fb3785ae7aaa63
Rudy Acosta <rudyacosta323@gmail.com>
2025-07-23 21:15:32 -0500

    First submission complete

---
+++ index.js
@@ -0,0 +1,21 @@
+//This is javascript
+
+var code1 = "code1";
+
+code1 = 2 * 5;
+
+var code2 = "code2";
+
+code2 = (100 - 20) / 2;
+
+var code3 = "code3";
+
+code3 = 100 % 61;
+
+var message = "The value of the vault has been secured. The combination is: ";
+
+const codeA = code1 + "-" + code2 + "-" + code3;
+
+const codeB = `${codeA}`;
+
+console.log(message, codeA, codeB);
